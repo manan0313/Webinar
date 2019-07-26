376e0dc402b2f98991deb58f9e87f4c94eaf3981
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-07-27 03:31:50 +0530

    Create

---
+++ app/src/main/java/com/example/webinar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webinar;
+
+import android.os.ConditionVariable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+        TextView tvResult;
+        EditText etVar1,etVar2;
+        Button btnAdd;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            etVar1 = findViewById(R.id.etVar1);
+            etVar2 = findViewById(R.id.etVar2);
+            btnAdd = findViewById(R.id.btnAdd);
+            tvResult = findViewById(R.id.tvResult);
+
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int var1 = Integer.valueOf(etVar1.getText().toString());
+                    int var2 = Integer.valueOf(etVar2.getText().toString());
+
+                    int var3 = var1 + var2;
+
+                    tvResult.setText(String.valueOf(var3));
+                }
+            });
+
+        }
+}
